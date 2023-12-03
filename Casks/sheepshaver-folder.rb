fe47518c55408d0b54de1cdd9aec8cc287ebc3f2
Eric Mika <eric@ericmika.com>
2023-12-03 17:22:21 -0500

    Clarify sheepshaver folder version.

--- Casks/sheepshaver-folder.rb
+++ Casks/sheepshaver-folder.rb
@@ -1,6 +1,8 @@
 cask "sheepshaver-folder" do
   # See https://www.emaculation.com/forum/viewtopic.php?f=20&t=7360
-  version "2.5"
+  # Versioning ambiguous, this is creation date of folder
+  version "2020.01.16"
+
   sha256 "ac714fed65354dc337026653d2e842ed9183ac93cc547ca46d26e531baad811f"
 
   url "https://ronaldpr.home.xs4all.nl/sheepshaverforum/SheepShaver_folder.zip"
