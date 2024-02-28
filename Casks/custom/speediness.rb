9de17c30fea233842498409c867cba872c598768
Eric Mika <eric@ericmika.com>
2024-02-27 20:46:47 -0500

    Update speediness.

--- Casks/custom/speediness.rb
+++ Casks/custom/speediness.rb
@@ -1,10 +1,10 @@
 cask "speediness" do
-  version "1.6.1"
-  sha256 "d800160cf694610f36ddab320b9b82aaf22b419c80db5fe8fb558696e1250d1e"
+  version "1.7.0"
+  sha256 "b90d7458a9326185a58df6ce0b2ae80cb4cb96ca728a9e9c045554f2cf72dbff"
 
   # Version in url is not going to survive a real update, but it works around
   # Homebrew's insistence on skipping checksums on unversioned URLs. ಠ_ಠ
-  url "https://www.dropbox.com/scl/fi/21mtkbqa4qqkz5ryiuu5t/Speediness-#{version}-1686472921-1701610430.zip?rlkey=bhqkthepo5lc3u7tqa0gsiywb&raw=1",
+  url "https://www.dropbox.com/scl/fi/fz7g9b5zjuvj0jehefuuv/Speediness-#{version}-1705505161.zip?rlkey=d1otns1la8mygh2a4uebb08pg&raw=1",
       verified: "dropbox.com/"
   name "Speediness"
   desc "Check your internet speed"
