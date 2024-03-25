71e86d4925291432894f961d7af2de8f8b55d968
Eric Mika <eric@ericmika.com>
2024-03-25 12:05:08 -0400

    Add Photos Workbench.

---
+++ Casks/custom/photos-workbench.rb
@@ -0,0 +1,11 @@
+cask "photos-workbench" do
+  version "1.2.5"
+  sha256 "8139d5a3326d8549e87d2cb98fe8c528e72d592f001f8895cf58a4322119b056"
+
+  url "https://www.houdah.com/photosWorkbench/download_assets/PhotosWorkbench#{version}.zip"
+  name "Photos Workbench"
+  desc "Organize, rate, and compare your photos"
+  homepage "https://www.houdah.com/photosWorkbench/"
+
+  app "Photos Workbench.app"
+end
