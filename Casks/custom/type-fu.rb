fd702f1325c45b936f37293124d1983d25e6fa40
Eric Mika <eric@ericmika.com>
2024-07-21 14:51:33 -0400

    Put R2 bucket behind domain.

--- Casks/custom/type-fu.rb
+++ Casks/custom/type-fu.rb
@@ -4,8 +4,8 @@ cask "type-fu" do
 
   # Version at end of the url is a dirty hack to work around Homebrew's
   # insistence on skipping checksums on unversioned URLs. ಠ_ಠ
-  url "https://pub-315235805cdf41349279caaaf80e8933.r2.dev/04835bf155028448746a979794f9c8c1a06d3f0bdff138cb88016b0b988181df.zip##{version}",
-      verified: "pub-315235805cdf41349279caaaf80e8933.r2.dev/"
+  url "https://storage.kitschpatrol.com/04835bf155028448746a979794f9c8c1a06d3f0bdff138cb88016b0b988181df.zip##{version}",
+      verified: "storage.kitschpatrol.com/"
   name "Type Fu"
   desc "Vintage native version of the typing training app"
   homepage "https://type-fu.com/"
