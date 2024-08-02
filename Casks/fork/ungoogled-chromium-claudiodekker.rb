6bf4d63bf5e646829607eda33c6c1e1c8cdc4bd9
Eric Mika <eric@ericmika.com>
2024-08-02 15:30:17 -0400

    Add signed ungoogled-chromium cask.

---
+++ Casks/fork/ungoogled-chromium-claudiodekker.rb
@@ -0,0 +1,39 @@
+cask "ungoogled-chromium-claudiodekker" do
+  arch arm: "arm64", intel: "x86-64"
+
+  version "127.0.6533.72-1.1"
+  sha256 arm:   "79e39609a064f2d1f4c0ba4c12c6a789acf767e18bdd95eea0e6c6fa9b06c90e",
+         intel: "961eca774c83f619f784a49e8dc11be4aef676874002b7b47211cb22935f4a19"
+  url "https://github.com/claudiodekker/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_#{arch}-macos-signed.dmg",
+      verified: "github.com/claudiodekker/ungoogled-chromium-macos/"
+  name "Ungoogled Chromium"
+  desc "Google Chromium, sans integration with Google signed and notarized by @claudiodekker."
+  homepage "https://ungoogled-software.github.io/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:[.-]\d+)+)(?:[._-]#{arch})?(?:[._-]+?(\d+(?:\.\d+)*))?$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
+  end
+
+  conflicts_with cask: [
+    "chromium",
+    "eloston-chromium",
+    "freesmug-chromium",
+  ]
+  depends_on macos: ">= :catalina"
+
+  app "Chromium.app"
+
+  zap trash: [
+    "~/Library/Application Support/Chromium",
+    "~/Library/Caches/Chromium",
+    "~/Library/Preferences/org.chromium.Chromium.plist",
+    "~/Library/Saved Application State/org.chromium.Chromium.savedState",
+  ]
+end
