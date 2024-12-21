42b3b5cf6e1c107cc8d8c6416cc643b908b17cef
Eric Mika <eric@ericmika.com>
2024-12-21 12:47:10 -0500

    Update Photos Workbench and add livecheck.

--- Casks/custom/photos-workbench.rb
+++ Casks/custom/photos-workbench.rb
@@ -1,11 +1,16 @@
 cask "photos-workbench" do
-  version "1.3.2"
-  sha256 "ff9fad49128e5b0a512fbbcc068e214c25b5e1fc3d62832bd7a5ebe6e0af9443"
+  version "1.3.3"
+  sha256 "dd9a5a4da05cf515fde95e35f3074098106c677a113e2423528828f8ed561dd4"
 
   url "https://www.houdah.com/photosWorkbench/download_assets/PhotosWorkbench#{version}.zip"
   name "Photos Workbench"
   desc "Organize, rate, and compare your photos"
   homepage "https://www.houdah.com/photosWorkbench/"
+
+  livecheck do
+    url "https://www.houdah.com/photosWorkbench/releaseNotes.html"
+    regex(/Photos Workbench (\d+(?:\.\d+)+)/i)
+  end
 
   app "Photos Workbench.app"
 end
